7d3d8d4b67024947368639ddcb367978819af813
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2024-02-24 14:32:04 -0300

    Update GerarData class.

--- demo/src/main/java/com/example/gerarDados.java
+++ demo/src/main/java/com/example/gerarDados.java
@@ -1,60 +1,83 @@
 package com.example;
+
 /* 
  * Gerar dados para a aplicação de forma aleatória, para testes sem repetição de numeros
  */
 import java.io.BufferedWriter; // Usa o Buffer interno para escrever no arquivo
+import java.io.File;
 import java.io.FileWriter; // Usa o FileWriter para escrever diretamente no arquivo
 import java.io.IOException; // Tratamento de exceções como erro de escrita
 import java.util.HashSet; // Usa o HashSet para garantir que não haja repetição de números
 import java.util.Set; // É usado para garantir que não haja repetição de números
 
-public class gerarDados {
-    public static void main(String[] args) {
-        System.out.println("Gerar Dados");
+public class GerarDados {
+    private int quantidadeNumeros;
+    private int[] numeros;
 
-        int quantidadeNumeros = 1000000; // Quantidade de números a serem gerados
+    public GerarDados(int quantidadeNumeros) {
+        this.quantidadeNumeros = quantidadeNumeros;
+        this.numeros = new int[quantidadeNumeros];
+    }
 
-        int[] numeros = new int[quantidadeNumeros];
+    private void gerarNumerosOrdenados() {
+        for (int i = 0; i < quantidadeNumeros; i++) {
+            numeros[i] = i;
+        }
+    }
 
-        // Gerar números aleatórios sem repetição
+    private void gerarNumerosOrdenadosInvertidos() {
+        for (int i = 0; i < quantidadeNumeros; i++) {
+            numeros[i] = quantidadeNumeros - i;
+        }
+    }
+
+    private void gerarNumerosAleatorios() {
         Set<Integer> numerosGerados = new HashSet<>();
-
-        // Gerar os números aleatórios sem repetição e armazenar no array
         for (int i = 0; i < quantidadeNumeros; i++) {
             int numero;
             do {
-                /* 
-                Gerar um número aleatório entre 0 e 999
-                math.random() gera um número entre 0 e 1000000
-                */
-
                 numero = (int) (Math.random() * quantidadeNumeros);
             } while (numerosGerados.contains(numero));
-            /* 
-            Contains verifica se o número já foi gerado
-            Se sim, gera um novo número, se não, adiciona o número ao array
-            */
             numerosGerados.add(numero);
-
-            // Adiciona o número ao array
             numeros[i] = numero;
         }
+    }
 
-        // Escrever os números no arquivo
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter("data/numerosAleatorios.txt"))) {
+    private void escreverNumerosArquivo(String nomeArquivo) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(nomeArquivo))) {
             for (int i = 0; i < quantidadeNumeros; i++) {
-                /* 
-                Escreve os números no arquivo
-                writer.write escreve o número no arquivo
-                Integer.toString converte o número para string
-                writer.newLine pula uma linha para o próximo número 
-                */
                 writer.write(Integer.toString(numeros[i]));
                 writer.newLine();
             }
-            System.out.println("Números escritos no arquivo 'numeros.txt' com sucesso.");
+            System.out.println("Números escritos no arquivo '" + nomeArquivo + "' com sucesso.");
         } catch (IOException e) {
             System.out.println("Ocorreu um erro ao escrever no arquivo: " + e.getMessage());
         }
+    }
+
+    private void CreateFolder(String folderName) {
+        File dataFolder = new File(folderName);
+        if (!dataFolder.exists()) {
+            boolean created = dataFolder.mkdirs();
+            if (!created) {
+                System.out.println("Falha ao criar a pasta 'data'.");
+                return;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Gerando números...");
+
+        GerarDados gerador = new GerarDados(1000000);
+
+        gerador.CreateFolder("data");
+
+        gerador.gerarNumerosOrdenados();
+        gerador.escreverNumerosArquivo("data/numerosOrdenados.txt");
+        gerador.gerarNumerosOrdenadosInvertidos();
+        gerador.escreverNumerosArquivo("data/numerosOrdenadosInvertidos.txt");
+        gerador.gerarNumerosAleatorios();
+        gerador.escreverNumerosArquivo("data/numerosAleatorios.txt");
     }
 }
