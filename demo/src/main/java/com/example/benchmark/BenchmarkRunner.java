60b27dcd71713f4fe65fc8ca351c875764b6903e
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2024-02-26 14:17:45 -0300

    Added benchmarks for the Select Sort algorithm.

--- demo/src/main/java/com/example/benchmark/BenchmarkRunner.java
+++ demo/src/main/java/com/example/benchmark/BenchmarkRunner.java
@@ -2,18 +2,18 @@ package com.example.Benchmark;
 
 public class BenchmarkRunner {
         public static void main(String[] args) throws Exception {
-                org.openjdk.jmh.Main.main(args);
+                //org.openjdk.jmh.Main.main(args);
 
                 /*
                  * -i 1: Quantas vezes será executado o teste.
                  * -wi 2: Quantas vezes será executado antes de começar a medir o tempo.
                  * -f 1: Quantas vezes será executado o teste.
                  */
-/* 
+
                 int quantidadeTestes = 1;
                 int quantidadeWarmup = 2;
                 int quantidadeMedicao = 1;
-
+/*
                 org.openjdk.jmh.Main.main(new String[] {
                                 "-i", Integer.toString(quantidadeTestes),
                                 "-wi", Integer.toString(quantidadeWarmup),
@@ -57,25 +57,45 @@ public class BenchmarkRunner {
                 });
 
                 org.openjdk.jmh.Main.main(new String[] {
-                                "-i", "1",
-                                "-wi", "2",
-                                "-f", "1",
+                                   "-i", Integer.toString(quantidadeTestes),
+                                "-wi", Integer.toString(quantidadeWarmup),
+                                "-f", Integer.toString(quantidadeMedicao),
                                 ".*BenchmarkBubbleSortOrdenadosV2.*"
                 });
 
                 org.openjdk.jmh.Main.main(new String[] {
-                                "-i", "1",
-                                "-wi", "2",
-                                "-f", "1",
+                                  "-i", Integer.toString(quantidadeTestes),
+                                "-wi", Integer.toString(quantidadeWarmup),
+                                "-f", Integer.toString(quantidadeMedicao),
                                 ".*BenchmarkBubbleSortInvertidoV2.*"
                 });
 
                 org.openjdk.jmh.Main.main(new String[] {
-                                "-i", "1",
-                                "-wi", "2",
-                                "-f", "1",
+                                 "-i", Integer.toString(quantidadeTestes),
+                                "-wi", Integer.toString(quantidadeWarmup),
+                                "-f", Integer.toString(quantidadeMedicao),
                                 ".*BenchmarkBubbleSortAleatorioV2.*"
                 });
  */
+                org.openjdk.jmh.Main.main(new String[] {
+                        "-i", Integer.toString(quantidadeTestes),
+                        "-wi", Integer.toString(quantidadeWarmup),
+                        "-f", Integer.toString(quantidadeMedicao),
+                                ".*BenchmarkSelectSortOrdenados.*"
+                });
+
+                org.openjdk.jmh.Main.main(new String[] {
+                        "-i", Integer.toString(quantidadeTestes),
+                        "-wi", Integer.toString(quantidadeWarmup),
+                        "-f", Integer.toString(quantidadeMedicao),
+                                ".*BenchmarkSelectSortInvertido.*"
+                });
+                
+                org.openjdk.jmh.Main.main(new String[] {
+                        "-i", Integer.toString(quantidadeTestes),
+                        "-wi", Integer.toString(quantidadeWarmup),
+                        "-f", Integer.toString(quantidadeMedicao),
+                                ".*BenchmarkSelectSortAleatorio.*"
+                });
         }
 }
