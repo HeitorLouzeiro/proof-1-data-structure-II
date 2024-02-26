efd8293a9c9876651a6cb67ecb5f205515aafb78
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2024-02-26 16:45:37 -0300

    Adds custom settings for running benchmarks.

--- demo/src/main/java/com/example/benchmark/BenchmarkRunner.java
+++ demo/src/main/java/com/example/benchmark/BenchmarkRunner.java
@@ -1,10 +1,64 @@
 package com.example.Benchmark;
 
 import org.openjdk.jmh.Main;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BenchmarkRunner {
+        private static final Map<String, Boolean> benchmarkClasses = new HashMap<>();
+
+        static {
+
+                // Adicione aqui as classes que deseja executar o benchmark
+
+                /*
+                 * Exemplo:
+                 * Classe: .*BenchmarkBubbleSortOrdenados.bubbleSort.*
+                 * Método: bubbleSort
+                 *
+                 * true: Executa o benchmark
+                 * false: Não executa o benchmark
+                 * 
+                 */
+
+                // Benchmark numeros ordenados
+                benchmarkClasses.put(".*BenchmarkBubbleSortOrdenados.bubbleSort.*", true);
+                benchmarkClasses.put(".*BenchmarkBubbleSortOrdenadosV1.*", false);
+                benchmarkClasses.put(".*BenchmarkBubbleSortOrdenadosV2.*", false);
+                benchmarkClasses.put(".*BenchmarkSelectSortOrdenados.*", false);
+                benchmarkClasses.put(".*BenchmarkMergeSortOrdenados.*", false);
+                benchmarkClasses.put(".*BenchmarkQuickSortOrdenados.*", false);
+                benchmarkClasses.put(".*BenchmarkHeapSortOrdenados.*", false);
+
+                // Benchmark numeros invertidos
+                benchmarkClasses.put(".*BenchmarkBubbleSortInvertido.bubbleSort.*", true);
+                benchmarkClasses.put(".*BenchmarkBubbleSortInvertidoV1.*", false);
+                benchmarkClasses.put(".*BenchmarkBubbleSortInvertidoV2.*", false);
+                benchmarkClasses.put(".*BenchmarkSelectSortInvertido.*", false);
+                benchmarkClasses.put(".*BenchmarkMergeSortInvertido.*", false);
+                benchmarkClasses.put(".*BenchmarkQuickSortInvertido.*", false);
+                benchmarkClasses.put(".*BenchmarkHeapSortInvertido.*", false);
+
+                // Benchmark numeros aleatorios
+                benchmarkClasses.put(".*BenchmarkBubbleSortAleatorio.bubbleSort.*", true);
+                benchmarkClasses.put(".*BenchmarkBubbleSortAleatorioV1.*", false);
+                benchmarkClasses.put(".*BenchmarkBubbleSortAleatorioV2.*", false);
+                benchmarkClasses.put(".*BenchmarkSelectSortAleatorio.*", false);
+                benchmarkClasses.put(".*BenchmarkMergeSortAleatorio.*", false);
+                benchmarkClasses.put(".*BenchmarkQuickSortAleatorio.*", false);
+                benchmarkClasses.put(".*BenchmarkHeapSortAleatorio.*", false);
+
+        }
+
         public static void main(String[] args) throws Exception {
-                // org.openjdk.jmh.Main.main(args);
+
+                /*
+                 * Executa o benchmark
+                 * org.openjdk.jmh.Main.main(args); Roda o benchmark com as configurações
+                 * padrão.
+                 */
+
+                //org.openjdk.jmh.Main.main(args);
 
                 /*
                  * -i 1: Quantas vezes será executado o teste.
@@ -12,24 +66,24 @@ public class BenchmarkRunner {
                  * -f 1: Quantas vezes será executado o teste.
                  */
 
-                int quantidadeTestes = 1;
+                // Executa o benchmark com as configurações personalizadas
+
+               int quantidadeTestes = 1;
                 int quantidadeWarmup = 2;
                 int quantidadeMedicao = 1;
 
-                String[] benchmarkClasses = {
-                                ".*BenchmarkBubbleSortOrdenados.bubbleSort.*",
-                                ".*BenchmarkBubbleSortInvertido.bubbleSort.*",
-                                ".*BenchmarkBubbleSortAleatorio.bubbleSort.*",
-
-                                // Adicione outras classes de benchmark aqui...
-                };
-                for (String benchmarkClass : benchmarkClasses) {
-                        Main.main(new String[] {
-                                        "-i", Integer.toString(quantidadeTestes),
-                                        "-wi", Integer.toString(quantidadeWarmup),
-                                        "-f", Integer.toString(quantidadeMedicao),
-                                        benchmarkClass
-                        });
+                for (Map.Entry<String, Boolean> entry : benchmarkClasses.entrySet()) {
+                        if (entry.getValue()) {
+                                Main.main(new String[] {
+                                                "-i", Integer.toString(quantidadeTestes),
+                                                "-wi", Integer.toString(quantidadeWarmup),
+                                                "-f", Integer.toString(quantidadeMedicao),
+                                                entry.getKey()
+                                });
+                        }
                 }
+
+                // Fim do benchmark com as configurações personalizadas
+
         }
 }
